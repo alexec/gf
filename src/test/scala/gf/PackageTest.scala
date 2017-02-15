85e173dcc7afe3ca718c032d5aaed9c6bfb0856e
Alex Collins <alex.e.c@gmail.com>
2017-02-15 15:01:54 +0000

    pit

---
+++ src/test/scala/gf/PackageTest.scala
@@ -0,0 +1,8 @@
+package gf
+
+import org.junit.Test
+import org.junit.Assert._
+
+class PackageTest {
+  @Test def wildIsZero() = assertEquals(0, wild)
+}
---
+++ src/test/scala/gf/SlotTest.scala
@@ -0,0 +1,19 @@
+package gf
+
+import org.junit.Assert._
+import org.junit.Test
+
+import scala.util.Random
+
+class SlotTest {
+  private val slot = new TestSlotFactory(() => new Random(0)).toGame(None)(Spin(BigDecimal(0)), NullWallet).get
+
+  @Test
+  def stops() = assertEquals(List(6, 3, 1, 2, 4), slot.stops)
+
+  @Test
+  def window() = assertEquals(List(List(9, 8, 4), List(6, 8, 1), List(1, 8, 2), List(2, 8, 5), List(6, 0, 1)), slot.window)
+
+  @Test
+  def payouts() = assertEquals(List((List(1, 1, 1, 1, 1), 3)), slot.payouts)
+}
