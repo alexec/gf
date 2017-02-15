71145767bb3c487498219a2f87b6c0b5b7e6702b
Alex Collins <alex.e.c@gmail.com>
2017-02-15 16:45:52 +0000

    extract

--- src/test/scala/gf/slot/SlotTest.scala
+++ src/test/scala/gf/slot/SlotTest.scala
@@ -5,14 +5,15 @@ import org.junit.Assert._
 import org.junit.Test
 
 class SlotTest {
-  private val slot = new TestSlotFactory(_ => List(6, 3, 1, 2, 4)).toGame(None)(Spin(BigDecimal(0)), NullWallet).get
+  private val stops = List(6, 3, 1, 2, 4)
+  private val slot = new TestSlotFactory(_ => stops).toGame(None)(Spin(BigDecimal(0)), NullWallet).get
 
   @Test
-  def stops() = assertEquals(List(6, 3, 1, 2, 4), slot.stops)
+  def stopsAreSame() = assertEquals(stops, slot.stops)
 
   @Test
-  def window() = assertEquals(List(List(9, 8, 4), List(6, 8, 1), List(1, 8, 2), List(2, 8, 5), List(6, 0, 1)), slot.window)
+  def windowIsAsExpected() = assertEquals(List(List(9, 8, 4), List(6, 8, 1), List(1, 8, 2), List(2, 8, 5), List(6, 0, 1)), slot.window)
 
   @Test
-  def payouts() = assertEquals(List((List(1, 1, 1, 1, 1), 3)), slot.payouts)
+  def payoutsAreAsExpected() = assertEquals(List((List(1, 1, 1, 1, 1), 3)), slot.payouts)
 }
