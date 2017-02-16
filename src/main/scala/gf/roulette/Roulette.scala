22e7c43775229b2bda0bab04fe1dd6d00e911496
Alex Collins <alex.e.c@gmail.com>
2017-02-16 09:09:10 +0000

    validate pocket

--- src/main/scala/gf/roulette/Roulette.scala
+++ src/main/scala/gf/roulette/Roulette.scala
@@ -3,19 +3,26 @@ package gf.roulette
 import gf.core.Wallet
 
 
-trait Bet {
+sealed case class Pocket(number: Int) {
+  require(number >= 0)
+  require(number <= 36)
+}
+
+sealed trait Bet {
   val amount: BigDecimal
 }
 
-case class NumberBet(amount: BigDecimal, pocket: Pocket) extends Bet
+case class NumberBet(amount: BigDecimal, pocket: Pocket) extends Bet {
+  require(pocket.number > 0)
+}
 
-case class Roulette(random: () => Pocket, wallet: Wallet, var pocket: Pocket = 0, var bets: List[Bet] = List()) {
-  def addBet(bet: Bet) = {
+case class Roulette(random: () => Pocket, wallet: Wallet, var pocket: Pocket = Pocket(0), var bets: List[Bet] = List()) {
+  def addBet(bet: Bet): Unit = {
     wallet.wager(bet.amount)
     bets = bet :: bets
   }
 
-  def spin() = {
+  def spin(): Unit = {
     pocket = random()
     val payouts = bets.map {
       case NumberBet(amount, pocket1) => if (pocket1 == pocket) Some(amount * 10) else None
--- src/test/scala/gf/roulette/RouletteTest.scala
+++ src/test/scala/gf/roulette/RouletteTest.scala
@@ -6,10 +6,30 @@ import org.junit.Test
 
 class RouletteTest {
   private val wallet = SimpleWallet()
-  private val roulette = Roulette(random = () => 1, wallet = wallet)
+  private val roulette = Roulette(random = () => Pocket(1), wallet = wallet)
+
+  @Test def pocketCanBeZero(): Unit = Pocket(0)
+
+
+  @Test def pocketCanBe36(): Unit = Pocket(36)
+
+  @Test(expected = classOf[IllegalArgumentException]) def pocketCannotBeNegative(): Unit =
+    Pocket(-1)
+
+  @Test(expected = classOf[IllegalArgumentException]) def pocketCannotBe37(): Unit =
+    Pocket(37)
+
+
+  @Test(expected = classOf[IllegalArgumentException]) def cannotBetOnZero() = {
+    roulette.addBet(NumberBet(BigDecimal(1), Pocket(0)))
+  }
+
+  @Test(expected = classOf[IllegalArgumentException]) def cannotBetOn37() = {
+    roulette.addBet(NumberBet(BigDecimal(1), Pocket(37)))
+  }
 
   @Test def addedBetIsAdded() = {
-    val bet = NumberBet(BigDecimal(10), 1)
+    val bet = NumberBet(BigDecimal(10), Pocket(1))
     roulette.addBet(bet)
     assertEquals(List(bet), roulette.bets)
   }
