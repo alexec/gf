13d2425aad6ac6c28d919fa040bec1957b9d3edf
Alex Collins <alex.e.c@gmail.com>
2017-02-16 09:00:05 +0000

    validate pocket

--- src/test/scala/gf/roulette/PackageTest.scala
+++ src/test/scala/gf/roulette/PackageTest.scala
@@ -1,5 +1,22 @@
 package gf.roulette
 
+import org.junit.Test
+
 class PackageTest {
 
+  @Test def pocketCannotBeZero(): Unit = {
+    Pocket(0)
+  }
+
+  @Test def pocketCannotBe36(): Unit = {
+    Pocket(36)
+  }
+
+  @Test(expected = classOf[IllegalArgumentException]) def pocketCannotBeNegative(): Unit = {
+    Pocket(-1)
+  }
+
+  @Test(expected = classOf[IllegalArgumentException]) def pocketCannotBe37(): Unit = {
+    Pocket(37)
+  }
 }
