223e09ec2c5375a41cdb68852c5d5606bf195a03
Alex Collins <alex.e.c@gmail.com>
2017-02-17 17:16:25 +0000

    validation

--- src/test/scala/gf/model/roulette/PocketTest.scala
+++ src/test/scala/gf/model/roulette/PocketTest.scala
@@ -5,25 +5,38 @@ import org.junit.Test
 
 class PocketTest {
 
-  @Test def pocketCanBeZero(): Unit = Pocket(0)
+  private val redNumbers = List(1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36)
+  private val blackNumbers = List(2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35).forall _
 
-  @Test def pocketCanBe36(): Unit = Pocket(36)
+  @Test def pocketCanBeZero(): Unit = Pocket(0)
 
   @Test def pocket0IsNotRed(): Unit = assertFalse(Pocket(0).isRed)
 
-  @Test def pocket19IsRed(): Unit = assertTrue(Pocket(19).isRed)
-
-  @Test def pocket33IsNotRed(): Unit = assertFalse(Pocket(33).isRed)
-
   @Test def pocket0IsNotBlack(): Unit = assertFalse(Pocket(0).isBlack)
 
-  @Test def pocket19IsNotRed(): Unit = assertFalse(Pocket(19).isBlack)
-
-  @Test def pocket33IsBlack(): Unit = assertTrue(Pocket(33).isBlack)
-
-  @Test(expected = classOf[IllegalArgumentException]) def pocketCannotBeNegative(): Unit = {
-    Pocket(-1)
-  }
+  @Test(expected = classOf[IllegalArgumentException]) def pocketCannotBeNegative(): Unit = Pocket(-1)
 
   @Test(expected = classOf[IllegalArgumentException]) def pocketCannotBe37(): Unit = Pocket(37)
+
+  @Test def redPocketsAreRed(): Unit =
+    assertTrue(redNumbers.forall {
+      Pocket(_).isRed
+    })
+
+  @Test def redPocketsAreNotBlack(): Unit =
+    assertFalse(redNumbers.forall {
+      Pocket(_).isBlack
+    })
+
+  @Test def blackPocketsAreBlack(): Unit =
+    assertTrue(blackNumbers {
+      Pocket(_).isBlack
+    })
+
+
+  @Test def blackPocketsAreNotRed(): Unit =
+    assertFalse(blackNumbers {
+      Pocket(_).isRed
+    })
+
 }
