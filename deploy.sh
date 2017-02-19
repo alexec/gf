68c17045a94e9cce057dfdd8a7673680b977be01
Alex Collins <alex.e.c@gmail.com>
2017-02-19 16:22:08 +0000

    test reports

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 set -eux
 
-ssh games.alexecollins.com "[ -e /var/gf ] || mkdir /var/gf"
-ssh games.alexecollins.com "adduser --system gf || true"
+H=root@games.alexecollins.com
 
-rsync -avz target/gf-1.0.0-SNAPSHOT.jar games.alexecollins.com:/var/gf/gf.jar
+ssh $H "[ -e /var/gf ] || mkdir /var/gf"
+ssh $H "adduser --system gf || true"
 
-ssh games.alexecollins.com "ln -sf /var/gf/gf.jar /etc/init.d/gf && chown gf /etc/init.d/gf && systemctl daemon-reload && systemctl start gf && systemctl restart gf"
+rsync -avz target/gf-1.0.0-SNAPSHOT.jar $H:/var/gf/gf.jar
+
+ssh $H "ln -sf /var/gf/gf.jar /etc/init.d/gf && chown gf /etc/init.d/gf && systemctl daemon-reload && systemctl start gf && systemctl restart gf"
