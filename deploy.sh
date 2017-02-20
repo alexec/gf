b2bb7b420419d94d63c2f50fa9ffc78015ff9c4f
Alex Collins <alex.e.c@gmail.com>
2017-02-20 14:30:38 +0000

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ ssh $H "adduser --system gf || true"
 
 rsync -avz --progress target/gf-1.0.0-SNAPSHOT.jar $H:/var/gf/gf.jar
 
-ssh $H "chmod +x /var/gf/gf.jar && ln -sf /var/gf/gf.jar /etc/init.d/gf && chown gf /etc/init.d/gf && systemctl daemon-reload && systemctl start gf && systemctl restart gf"
+ssh $H "chmod +x /var/gf/gf.jar && ln -sf /var/gf/gf.jar /etc/init.d/gf && chown gf /etc/init.d/gf && /etc/init.d/gf restart"
