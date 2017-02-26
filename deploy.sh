ff3144cbf698c77047540cd42579dc33ff38fa73
Alex Collins <alex.e.c@gmail.com>
2017-02-26 19:37:56 +0000

    phoebus

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 set -eux
 
 H=root@games.alexecollins.com
-D=/var/helios-games
+D=/var/phoebus-games
 U=gf
 
 ssh $H "[ -e $D ] || mkdir $D"
 ssh $H "adduser --system $U || true"
 
-rsync -avz --progress target/$U.jar $H:/var/helios-games/$U.jar
+rsync -avz --progress target/$U.jar $H:/var/phoebus-games/$U.jar
 
 ssh $H "echo > $D/$U.conf && chmod +x $D/$U.jar && ln -sf $D/$U.jar /etc/init.d/$U && chown $U /etc/init.d/$U && /etc/init.d/$U restart"
