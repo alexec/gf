053e1e1875eeef5c6cbecfb20f76e44ed59c532e
Alex Collins <alex.e.c@gmail.com>
2017-05-27 11:45:05 +0100

    print full stack trace

--- game-support/src/main/scala/games/app/ServerErrorExceptionMapper.scala
+++ game-support/src/main/scala/games/app/ServerErrorExceptionMapper.scala
@@ -3,12 +3,14 @@ package games.app
 import javax.ws.rs.core.Response
 import javax.ws.rs.ext.{ExceptionMapper, Provider}
 
+import org.glassfish.jersey.internal.util.ExceptionUtils
+
 
 @Provider class ServerErrorExceptionMapper extends ExceptionMapper[Exception] {
   override def toResponse(ex: Exception): Response =
     Response
       .status(500)
-      .entity(ex.getMessage)
+      .entity(ExceptionUtils.exceptionStackTraceAsString(ex))
       .`type`("text/plain")
       .build
 }
